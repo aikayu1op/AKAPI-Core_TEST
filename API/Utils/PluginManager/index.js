4a4fd0ddaedb5d3eaedcf29e2dc4b5b525e5e237
aikayu1op <58659725+aikayu1op@users.noreply.github.com>
2025-06-11 17:02:28 +0900

    update API/Utils/PluginManager/index.js

--- API/Utils/PluginManager/index.js
+++ API/Utils/PluginManager/index.js
@@ -6,10 +6,12 @@ let allplugins = "";
 let errorlist = [];
 let minus = 0;
 let index = 0;
+/** @type {string[]} */
 let files;
+let pluginFolder = {};
 export class Plugins {
   /**
-   * プラグインを読み込みます。
+   * プラグインを読み込みます。フォルダの名前がAPIと最後についている場合は、APIとして読み込み、getやgetAllで呼び出せるようになります。
    * @param {string[]} filenames
    * @param {boolean} debug 読み込んだプラグインを表示します。
    */
@@ -25,9 +27,24 @@ export class Plugins {
       if (data.includes("author: ")) author = data.split(":")[1].trim();
       else if (filename.length == 2) {
         let time = Date.now();
-        import(`../../../../../${filename[0]}.js`)
-        .then(() => {
+        // APIとして読み込むかどうか
+        let isAPI = filename[0].endsWith("API");
+        import(`../../../../../${filename[0]}/index.js`)
+        .then((plugin) => {
           let now = Date.now();
+          if(isAPI){
+            Object.entries(plugin).map(([key, value]) => {
+              // コメント・改行・インデント削除
+              const cleanedCode = value.toString()
+                .replace(/\/\*[\s\S]*?\*\//g, '')      // /* コメント */
+                .replace(/\/\/.*$/gm, '')              // // コメント
+                .replace(/^\s+/gm, '')                 // インデント
+                .replace(/\r?\n|\r/g, '');             // 改行
+
+                // クラスを return する形に変換し、即時実行
+              pluginFolder[key] = new Function(`${cleanedCode}; return ${key};`)();
+            });
+          }
           allplugins += `§a${filename[0]} ver${filename[1]} §f${now - time}ms\n`;
           if(debug) console.warn("load plugins: "+filename[0]);
         })
@@ -41,7 +58,7 @@ export class Plugins {
         });
       } else {
         let time = Date.now();
-        import(`../../../../../${data}`)
+        import(`../../../../../${data}/index.js`)
         .then(() => {
           let now = Date.now();
           allplugins += `§a${data} ver? §f${now - time}ms\n`;
@@ -57,6 +74,32 @@ export class Plugins {
       }
       index++;
     })
+  }
+  /**
+   * 
+   * @param {string} pluginName 
+   * @returns {boolean}
+   * @description 指定したプラグインが存在するかどうかを確認します。
+   */
+  static isExist(pluginName){
+    return pluginFolder.hasOwnProperty(pluginName);
+  }
+  /**
+   * @description 指定したプラグインを取得します。
+   * @param {string} pluginName 
+   * @returns {Class}
+   */
+  static getPlugin(pluginName){
+      if(!pluginFolder[pluginName]) throw new Error(`Plugin ${pluginName} is not found.`);
+      return pluginFolder[pluginName];
+  }
+  /**
+   * 
+   * @returns {string[]}
+   * @description 登録されている全てのプラグインの名前を取得します。
+   */
+  static getAllPlugins(){
+      return Object.entries(pluginFolder).map(([name, _]) => name);
   }
 }
 
